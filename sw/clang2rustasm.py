0041f9d7558c5fd4b52663428a13e33ceeec0a6f
Jade Philipoom <jade.philipoom@gmail.com>
2026-06-15 16:24:15 +0200

    Change to strip anything starting with .cfi.

--- sw/clang2rustasm.py
+++ sw/clang2rustasm.py
@@ -202,7 +202,7 @@ def extract_all_code(lines):
     in_code_section = False
     in_function = False
 
-    skip_in_body = {".globl", ".type ", ".size ", ".p2align", ".cfi_startproc"}
+    skip_in_body = {".globl", ".type ", ".size ", ".p2align", ".cfi"}
 
     for line in lines:
         stripped = line.strip()
